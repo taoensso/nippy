677859c35e0b213547afaab6791e6f51edf8c633
Peter Taoussanis <ptaoussanis@gmail.com>
2012-09-27 15:29:33 +0700

    Bump Snappy dependency (1.0.5-M2)
    Fixes issue: https://github.com/xerial/snappy-java/issues/6#issuecomment-8898271

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ptaoussanis/nippy"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure           "1.3.0"]
-                 [org.xerial.snappy/snappy-java "1.0.5-M1"]]
+                 [org.xerial.snappy/snappy-java "1.0.5-M2"]]
   :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
