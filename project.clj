52ebdada107564f781a1ac21bee57a0253b15e4b
Peter Taoussanis <ptaoussanis@gmail.com>
2012-12-04 13:20:08 +0700

    Bump version (fix)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/nippy "1.0.0"
+(defproject com.taoensso/nippy "1.0.1-SNAPSHOT"
   :description "Clojure serialization library"
   :url "https://github.com/ptaoussanis/nippy"
   :license {:name "Eclipse Public License"}
