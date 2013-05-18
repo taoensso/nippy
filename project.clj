f9a149ee1fbf95af6b617f7a69502d6ce7c294f1
Peter Taoussanis <ptaoussanis@gmail.com>
2013-05-18 16:54:57 +0700

    Bump Clojure 1.5 test dependency (1.5.1)

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.iq80.snappy/snappy "0.3"]]
   :profiles {:1.3  {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0-alpha3"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :dev  {:dependencies []}
              :test {:dependencies [[org.xerial.snappy/snappy-java "1.0.5-M3"]]}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
