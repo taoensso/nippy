fb7022a398836372fa79aa3c7f38702f7ef795a9
Peter Taoussanis <ptaoussanis@gmail.com>
2013-06-01 19:15:31 +0700

    Add Codox plugin to project.clj

--- project.clj
+++ project.clj
@@ -10,5 +10,6 @@
              :dev  {:dependencies []}
              :test {:dependencies [[org.xerial.snappy/snappy-java "1.0.5-M3"]]}}
   :aliases {"test-all" ["with-profile" "test,1.3:test,1.4:test,1.5" "test"]}
+  :plugins [[codox "0.6.4"]]
   :min-lein-version "2.0.0"
   :warn-on-reflection true)
