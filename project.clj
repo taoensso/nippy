52dea4dff81a54b8ec99af9a60862a3389e4a81a
Peter Taoussanis <ptaoussanis@gmail.com>
2013-07-09 13:25:49 +0700

    Bump non-core deps

--- project.clj
+++ project.clj
@@ -4,21 +4,22 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure    "1.4.0"]
-                 [expectations           "1.4.48"]
+                 [expectations           "1.4.49"]
                  [org.iq80.snappy/snappy "0.3"]]
   :profiles {:1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6   {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev   {:dependencies []}
-             :test  {:dependencies [[org.xerial.snappy/snappy-java "1.0.5-M3"]]}
+             :test  {:dependencies [[org.xerial.snappy/snappy-java "1.0.5"]]}
              :bench {:dependencies [] :jvm-opts ^:replace ["-server"]}}
   :aliases {"test-all"    ["with-profile" "test,1.4:test,1.5:test,1.6" "expectations"]
             "test-auto"   ["with-profile" "test" "autoexpect"]
             "start-dev"   ["with-profile" "dev,test,bench" "repl" ":headless"]
             "start-bench" ["trampoline" "start-dev"]
             "codox"       ["with-profile" "test" "doc"]}
-  :plugins [[lein-expectations "0.0.7"]
+  :plugins [[lein-expectations "0.0.8"]
             [lein-autoexpect   "0.2.5"]
+            [lein-ancient      "0.4.2"]
             [codox             "0.6.4"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
