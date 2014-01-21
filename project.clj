f5544cae166aa3ced8a00ee3dba0749f59a198ff
Peter Taoussanis <ptaoussanis@gmail.com>
2014-01-21 14:24:02 +0700

    Deps: tools.reader 0.8.3, lein-autoexpect 1.2.1, (new) simple-check 0.5.3

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure      "1.4.0"]
-                 [org.clojure/tools.reader "0.7.10"]
+                 [org.clojure/tools.reader "0.8.3"]
                  [org.iq80.snappy/snappy   "0.3"]
                  [org.tukaani/xz           "1.4"]]
   :profiles {:1.4   {:dependencies [[org.clojure/clojure "1.4.0"]]}
@@ -12,7 +12,8 @@
              :1.6   {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]}
              :dev   {:dependencies []}
              :test  {:dependencies [[expectations                  "1.4.56"]
-                                    [org.xerial.snappy/snappy-java "1.1.1-M1"]]}
+                                    [org.xerial.snappy/snappy-java "1.1.1-M1"]
+                                    [reiddraper/simple-check       "0.5.3"]]}
              :bench {:dependencies [] :jvm-opts ^:replace ["-server"]}}
   :aliases {"test-all"    ["with-profile" "+test,+1.4:+test,+1.5:+test,+1.6" "expectations"]
             "test-auto"   ["with-profile" "+test" "autoexpect"]
@@ -20,7 +21,7 @@
             "start-bench" ["trampoline" "start-dev"]
             "codox"       ["with-profile" "+test" "doc"]}
   :plugins [[lein-expectations "0.0.8"]
-            [lein-autoexpect   "1.0"]
+            [lein-autoexpect   "1.2.1"]
             [lein-ancient      "0.5.4"]
             [codox             "0.6.6"]]
   :min-lein-version "2.0.0"
