9a9330ed51095ffd7929c1b8a8fea4d8f24ce0d7
Peter Taoussanis <ptaoussanis@gmail.com>
2014-07-05 14:43:24 +0700

    Update project.clj template

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
   :min-lein-version "2.3.3"
   :global-vars {*warn-on-reflection* true
                 *assert* true}
+
   :dependencies
   [[org.clojure/clojure      "1.4.0"]
    [org.clojure/tools.reader "0.8.5"]
@@ -17,37 +18,33 @@
    [org.tukaani/xz           "1.5"]
    [net.jpountz.lz4/lz4      "1.2.0"]]
 
-  :test-paths ["test" "src"]
   :profiles
   {;; :default [:base :system :user :provided :dev]
+   :server-jvm {:jvm-opts ^:replace ["-server" "-Xms1024m" "-Xmx2048m"]}
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :test {:jvm-opts     ["-Xms1024m" "-Xmx2048m"]
-          :dependencies [[expectations                  "1.4.56"]
+          :dependencies [[expectations                  "2.0.7"]
                          [org.clojure/test.check        "0.5.8"]
+                         ;; [com.cemerick/double-check  "0.5.7"]
                          [org.clojure/data.fressian     "0.2.0"]
-                         [org.xerial.snappy/snappy-java "1.1.1"]]
-          :plugins [[lein-expectations "0.0.8"]
-                    [lein-autoexpect   "1.2.2"]]}
-   :dev* [:dev {:jvm-opts ^:replace ["-server"]
-                ;; :hooks [cljx.hooks leiningen.cljsbuild] ; cljx
-                }]
-   :dev
-   [:1.6 :test
-    {:jvm-opts ^:replace ["-server" "-Xms1024m" "-Xmx2048m"]
-     :dependencies []
-     :plugins [[lein-ancient "0.5.4"]
-               [codox        "0.6.7"]]}]}
+                         [org.xerial.snappy/snappy-java "1.1.1"]]}
+   :dev [:1.6 :test
+         {:plugins
+          [[lein-pprint       "1.1.1"]
+           [lein-ancient      "0.5.5"]
+           [lein-expectations "0.0.8"]
+           [lein-autoexpect   "1.2.2"]
+           [codox             "0.8.10"]]}]}
 
-  ;; :codox {:sources ["target/classes"]} ; cljx
+  :test-paths ["test" "src"]
+
   :aliases
   {"test-all"   ["with-profile" "default:+1.5:+1.6" "expectations"]
    ;; "test-all"   ["with-profile" "default:+1.6" "expectations"]
    "test-auto"  ["with-profile" "+test" "autoexpect"]
-   ;; "build-once" ["do" "cljx" "once," "cljsbuild" "once"] ; cljx
-   ;; "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"] ; cljx
    "deploy-lib" ["do" "deploy" "clojars," "install"]
-   "start-dev"  ["with-profile" "+dev*" "repl" ":headless"]}
+   "start-dev"  ["with-profile" "+server-jvm" "repl" ":headless"]}
 
   :repositories
   {"sonatype"
