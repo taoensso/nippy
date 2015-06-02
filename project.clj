12d90a05f8d5719e07b624476dd7c1253425c3a4
Peter Taoussanis <ptaoussanis@gmail.com>
2015-06-02 12:45:57 +0700

    Bump dev Clojure version

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
    :server-jvm {:jvm-opts ^:replace ["-server" "-Xms1024m" "-Xmx2048m"]}
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}
+   :1.7  {:dependencies [[org.clojure/clojure "1.7.0-RC1"]]}
    :test {:jvm-opts     ["-Xms1024m" "-Xmx2048m"]
           :dependencies [[expectations                  "2.1.1"]
                          [org.clojure/test.check        "0.7.0"]
