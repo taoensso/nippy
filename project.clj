8989df5c3dace07a363121358c5cfb5850a12581
Peter Taoussanis <ptaoussanis@gmail.com>
2015-09-28 18:50:10 +0700

    v2.11.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/nippy "2.10.0"
+(defproject com.taoensso/nippy "2.11.0-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure serialization library"
   :url "https://github.com/ptaoussanis/nippy"
