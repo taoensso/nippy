5e93d4803270cf2b32fc5c8b92c016762acb923c
Peter Taoussanis <ptaoussanis@gmail.com>
2016-01-23 11:59:48 +0700

    Clojure 1.8.0 is out

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}
+   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :test {:jvm-opts     ["-Xms1024m" "-Xmx2048m"]
           :dependencies [[org.clojure/test.check        "0.9.0"]
                          [org.clojure/data.fressian     "0.2.1"]
