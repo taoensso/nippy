809bcdc6495ac345c4a6b8c05c49828935f96c6f
Peter Taoussanis <ptaoussanis@gmail.com>
2020-06-24 12:22:51 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -15,10 +15,10 @@
   :dependencies
   [[org.clojure/clojure      "1.5.1"]
    [org.clojure/tools.reader "1.3.2"]
-   [com.taoensso/encore      "2.106.0"]
+   [com.taoensso/encore      "2.122.0"]
    [org.iq80.snappy/snappy   "0.4"]
    [org.tukaani/xz           "1.8"]
-   [org.lz4/lz4-java         "1.5.0"]]
+   [org.lz4/lz4-java         "1.7.1"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
@@ -28,16 +28,16 @@
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
    :test {:jvm-opts     ["-Xms1024m" "-Xmx2048m"]
-          :dependencies [[org.clojure/test.check        "0.9.0"]
-                         [org.clojure/data.fressian     "0.2.1"]
-                         [org.xerial.snappy/snappy-java "1.1.7.2"]]}
+          :dependencies [[org.clojure/test.check        "1.1.0"]
+                         [org.clojure/data.fressian     "1.0.0"]
+                         [org.xerial.snappy/snappy-java "1.1.7.6"]]}
    :dev [:1.10 :test :server-jvm
          {:plugins
-          [[lein-pprint  "1.2.0"]
+          [[lein-pprint  "1.3.2"]
            [lein-ancient "0.6.15"]
-           [lein-codox   "0.10.6"]]}]}
+           [lein-codox   "0.10.7"]]}]}
 
   :test-paths ["test" "src"]
 
